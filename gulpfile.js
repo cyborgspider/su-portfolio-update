bbc4e4a1307996f159425d933a48899719b4372f
Carlos Ortega <carlos@cyborgspider.com>
2015-04-18 23:58:49 -0700

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp        = require('gulp');
+var $           = require('gulp-load-plugins')({lazy:true});
+var browserSync = require('browser-sync');
+var nib         = require('nib');
+var jeet        = require('jeet');
+var clean       = require('del');
+
+gulp.task('server', function(){
+  browserSync({
+    server:{
+      baseDir: "./build/"
+    }
+  })
+});
+
+gulp.task('style', function(){
+  return gulp
+    .src('./src/styles/styles.styl')
+    .pipe($.sourcemaps.init())
+    .pipe($.stylus({
+      compress:true,
+      use: [nib(),jeet()]
+    }))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe($.livereload())
+});
+
+gulp.task('jade', function(){
+  return gulp
+    .src('./src/*.jade')
+    .pipe($.jade())
+    .pipe(gulp.dest('./build'))
+    .pipe($.livereload())
+});
+
+gulp.task('clean', function(){
+  clean('./build', {read:false})
+});
+
+gulp.task('copy', function(){
+  gulp
+    .src('./src/scripts/*.js')
+    .pipe(gulp.dest('./build/js/'))
+    .pipe($.livereload());           
+  gulp
+    .src('./src/styles/*.css')
+    .pipe(gulp.dest('./build/css/')) ;
+  gulp
+    .src('./src/images/*')
+    .pipe(gulp.dest('./build/img/'));
+});
+
+gulp.task('default', function(){
+  console.log('Building, watching and starting server...');
+  gulp.start('jade', 'style', 'copy','server', 'watch');
+});
+
+gulp.task('watch', function(){
+  $.livereload.listen();
+  gulp.watch('./src/*.jade', ['jade']);
+  gulp.watch('./src/styles/*.styl', ['style']);
+  gulp.watch('./src/scripts/*.js', ['copy']);
+});
+
+gulp.task('build', function(){
+  gulp.start('jade', 'style', 'copy');
+  console.log('Your files have been built, thanks!');
+});
---
+++ src/scripts/scripts.js
@@ -0,0 +1,37 @@
+(function(){
+
+	//Create an empty array where all our strings will be contained
+	var content = [];
+
+	//Go through each INFO block on the page and create an HTML string and put into the content array
+	$('.info').each(function(i){
+		var $this          = $(this),
+			projectTitle   = this.hash.substr(1);
+			numberOfImages = $this.attr('data-number'),
+			imageFilename  = $this.attr('data-filename'),
+			imgArray = [];
+
+		for (var i = 0; i < numberOfImages ; i++){
+			imgArray.push('<img src="img/' + imageFilename + '_' + i + '.jpg" alt="" />')
+		}
+
+		var productTemplate =   '<div class="modal-content mfp-hide" id="'+ projectTitle + '">' + 
+								'<h2>' + projectTitle + '</h2>' + 
+								imgArray.join('') + '</div>';
+		
+		//Adding to the global content array
+		content.push(productTemplate)
+	});
+	//Here's the code that adds the content to our page.
+	$(content.join('')).appendTo('body');
+
+	//Modal lightbox code, only if the content has been loaded
+	if (content){
+		$('.info').magnificPopup({
+			type:'inline'
+		});
+	} else {
+		console.log('Please wait while our content is generated.')
+	}
+
+})();
