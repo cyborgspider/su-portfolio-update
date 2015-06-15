4dd0443fdccb80502d5301af8d058db3ef6701bc
Carlos Ortega <carlos@goodrx.com>
2015-06-15 12:53:06 -0700

    add wayfinding JS, add navigation fish-hook animation

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -73,17 +73,61 @@
 	    });
 	});
 
-	//Animated navigation
-	$('.nav-links').on('click', 'a', function(e){
-     	var $this        = $(this),
-     		$siblings    = $('.nav-links').find('a'),
-     		target       = $this.attr('href'),
-         	targetOffset = $(target).offset().top - 80;
+	//Scrollspy (Wayfinder.js)
+	// Cache selectors
+	var lastId,
+	    topMenu = $(".nav-links"),
+	    topMenuHeight = topMenu.outerHeight()+15,
+	    // All list items
+	    menuItems = topMenu.find("a"),
+	    // Anchors corresponding to menu items
+	    scrollItems = menuItems.map(function(){
+	      var item = $($(this).attr("href"));
+	      if (item.length) { return item; }
 
-    	e.preventDefault();
-    	$siblings.removeClass('active');
-    	$this.addClass('active');
-    	$('html,body').animate({scrollTop: targetOffset + 'px'}, 700);
-	})
+	    });
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e){
+	  var href = $(this).attr("href"),
+	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+	  $('html, body').stop().animate({
+	      scrollTop: offsetTop
+	  }, 300);
+	  e.preventDefault();
+	});
+
+	// Bind to scroll
+	$(window).scroll(function(){
+	   // Get container scroll position
+	   var fromTop = $(this).scrollTop()+topMenuHeight;
+	   console.log(id);
+
+	   // Get id of current scroll item
+	   var cur = scrollItems.map(function(){
+	     if ($(this).offset().top < fromTop)
+	       return this;
+	   });
+	   // Get the id of the current element
+	   cur = cur[cur.length-1];
+	   var id = cur && cur.length ? cur[0].id : "";
+
+	   if (lastId !== id) {
+	       lastId = id;
+
+	       // Set/remove active class
+	       menuItems
+	         .removeClass("active")
+	         .filter("[href=#"+id+"]").addClass("active");
+
+	       //Handle the fish-hook nav animation
+	       if(id !== 'hero'){
+	       	$('.nav-animation').addClass('active');
+	       } else {
+	       	$('.nav-animation').removeClass('active');
+	       };
+	   }
+
+	});
 
 })();
